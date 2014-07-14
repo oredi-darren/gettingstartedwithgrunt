0486063d2f02982e3f2995d8f55657b5f52a51db
oredi <darren_seet@oredi.com>
2014-07-15 02:03:47 +0900

    Complete chapter 4 example 05

---
+++ chapter4/example4_05/project/Gruntfile.js
@@ -0,0 +1,85 @@
+module.exports = function(grunt) {
+    // Environment specific tasks
+    grunt.loadNpmTasks('grunt-contrib-coffee');
+    grunt.loadNpmTasks('grunt-contrib-stylus');
+    grunt.loadNpmTasks('grunt-contrib-jade');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+
+    // Project configuration
+    grunt.initConfig({
+        coffee: {
+            build: {
+                options: {
+                    join: true
+                },
+                src: [
+                    "src/scripts/**/*.coffee",
+                    "!src/scripts/app.coffee",
+                    "src/scripts/app.coffee"
+                ],
+                dest: "build/js/app.js"
+            }
+        },
+        stylus: {
+            options: {
+                compress: false
+            },
+            build: {
+                src: "src/styles/app.styl",
+                dest: "build/css/app.css"
+            }
+        },
+        jade: {
+            build: {
+                options: {
+                    pretty: true
+                },
+                src: "src/views/app.jade",
+                dest: "build/app.html"
+            }
+        },
+        uglify: {
+            compress: {
+                src: "<%= coffee.build.dest %>",
+                dest: "<%= coffee.build.dest %>"
+            }
+        },
+        cssmin: {
+            compress: {
+                src: "<%= stylus.build.dest %>",
+                dest: "<%= stylus.build.dest %>"
+            }
+        },
+        htmlmin: {
+            options: {
+                removeComments: true,
+                collapseWhitespace: true,
+                collapseBooleanAttributes: true,
+                removeAttributeQuotes: true,
+                removeRedundantAttributes: true,
+                removeOptionaltags: true
+            },
+            compress: {
+                src: "<%= jade.build.dest %>",
+                dest: "<%= jade.build.dest %>"
+            }
+        }
+    });
+
+    // Initialize environment
+    var env = grunt.option('env') || 'dev';
+    if(env === 'prod') {
+        grunt.registerTask('scripts', ['coffee', 'uglify']);
+        grunt.registerTask('styles', ['stylus', 'cssmin']);
+        grunt.registerTask('views', ['jade', 'htmlmin']);
+    } else {
+        grunt.registerTask('scripts', ['coffee']);
+        grunt.registerTask('styles', ['stylus']);
+        grunt.registerTask('views', ['jade']);
+    }
+
+    // Define the default task
+    grunt.registerTask('default', ['scripts', 'styles', 'views'])
+};
